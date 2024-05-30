23c4ef546e499517f6244a626b917d3a45323320
Jonathanfajardo <jonnyfajardo18@gmail.com>
2024-05-29 20:36:44 -0400

    initialized the project with react, installed a directory with components; navbar, footer, home page, petlist, petdetails, userprofile. added some placeholder code in the components and running localhost now displays some very basic elements

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Finding Fido</h1>
+      <p>Adopt a family member today!</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ client/src/components/PetList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PetList() {
+  return (
+    <div>
+      <h1>List of Pets</h1>
+      
+    </div>
+  );
+}
+
+export default PetList;
